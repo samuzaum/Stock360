eff4eb3ad872efd5f9763588b1346d054872d26e
samuzaum <samuelouvera@gmail.com>
2024-08-19 17:12:37 -0300

    update /page marcos

--- js/loadContent.js
+++ js/loadContent.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $(".asidelink").click(function(e){
         e.preventDefault();
-        
+
         var page = $(this).data("page");
         
-        $("#main-content").load("pages/" + page + ".html");
+        $("#main-content").load("pages/" + page + ".html", function() {
+        $("#main-content").attr('class', page + "-page");
+        });
     });
 });
