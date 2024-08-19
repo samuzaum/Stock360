9dd8572e5e361d81a064c41b44feb3e561354dea
Davi Garcia Castro Alves <davidanivi@gmail.com>
2024-08-19 20:59:49 -0300

    adição do javascript e mudanças no css. 19/08/2024

---
+++ js/pedidos.js
@@ -0,0 +1,25 @@
+ 
+ function aumentarNumero() {
+    let quantidadeInput = document.getElementById('quantidade-pedidos-input');
+    
+    // Converte o valor atual para um número inteiro
+    let quantidadeAtual = parseInt(quantidadeInput.value, 10);
+    
+    // Incrementa o valor atual
+    quantidadeAtual++;
+    
+    // Atualiza o campo com o novo valor
+    quantidadeInput.value = quantidadeAtual;
+}
+
+function diminuirNumero() {
+    let quantidadeInput = document.getElementById('quantidade-pedidos-input');
+    let quantidadeAtual = parseInt(quantidadeInput.value, 10);
+    
+    if(quantidadeAtual == 0){
+        quantidadeAtual = 0
+    }else {
+    quantidadeAtual--;
+    quantidadeInput.value = quantidadeAtual;
+    }
+}
