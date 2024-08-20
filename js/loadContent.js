592264630b899d86d1fec7c95b3c374e83c12849
Samuel Henrique <samuelouvera@gmail.com>
2024-08-20 15:03:11 -0300

    Aside att e removendo bgl antigo

--- js/loadContent.js
+++ js/loadContent.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+    // Carregar a página inicial por padrão
+    $("#main-content").load("pages/inicio.html");
+    $(".asidelink[data-page='inicio']").addClass("active");
+    // Adicionar a classe 'active' ao link clicado
     $(".asidelink").click(function(e){
         e.preventDefault();
+        $(".asidelink").removeClass("active");
+        $(this).addClass("active");
         
         var page = $(this).data("page");
-        
-        $("#main-content").load("pages/" + page + ".html");
+        $("#main-content").addClass("fade");
+        setTimeout(function() {
+            $("#main-content").load("pages/" + page + ".html", function() {
+                $("#main-content").removeClass("fade");
+            });
+        }, 300);
     });
 });
